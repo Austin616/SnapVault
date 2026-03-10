6e6072bf4e869030e827a97cf6ee1920d2a2b7d1
austin616 <austintran616@gmail.com>
2026-03-10 15:30:37 -0500

    init

---
+++ SnapVault/SnapVaultApp.swift
@@ -0,0 +1,17 @@
+//
+//  SnapVaultApp.swift
+//  SnapVault
+//
+//  Created by Austin Tran on 3/10/26.
+//
+
+import SwiftUI
+
+@main
+struct SnapVaultApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ SnapVaultTests/SnapVaultTests.swift
@@ -0,0 +1,17 @@
+//
+//  SnapVaultTests.swift
+//  SnapVaultTests
+//
+//  Created by Austin Tran on 3/10/26.
+//
+
+import Testing
+@testable import SnapVault
+
+struct SnapVaultTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
